b38a54e9e45aebb6104aa887e06a56d66de23e0b
Greg Bulmash <burgerguy@gmail.com>
2025-01-11 22:34:21 -0800

    MLP completed

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ Use your favorite method to copy that export folder into your phone's Music fold
 
 ## V0.0.1
 
-"Developers Only" version: You need to know how to edit a JSON file.
+"Developers Only" version: A proof of concept that will be difficult for non-developers to run.
 
 ### Installation
-REQUIRED: Node.js, this has oinly been tested with 22 and above, but it uses very simple API calls and should have decent backward compatibility.
+REQUIRED: Node.js, this has only been tested with 18.71, but it uses very simple API calls, has 2 NPM dependencies (that install a total of 4 packages), and should have decent backward/forward compatibility.
 
 - Clone this repository from Github
 - Go into the directory it imported was copied into and run the command `npm install` in the console.
@@ -28,7 +28,18 @@ REQUIRED: Node.js, this has oinly been tested with 22 and above, but it uses ver
 
 This is a simple command line version. The commands are as follows:
 
-- `index.js analyze` - Analyzes your library and exports a list of playlists to `playlists.json`
+- `index.js analyze` - exports a list of playlists to `playlists.json`
   - Analyzes the XML file produced when you export your library from Apple Music
-  - Location of the file is specified in .env
+  - Location of the file is specified in .env if you don't want to put Library.xml in the code directory
 - `index.js export` - imports the `playlists.json` file and then copies the unique music files and writes M3U playlists to the folder specified in the .env file.
+
+Copy the folder full of songs and .m3u files to the Music folder of your Android phone and many music players should pick it all up with no problem.
+
+### Known problems
+
+Some file names don't seem to transfer, or they do, but don't show up in the playlists. We'll need to examine that for later updates.
+
+### Possible Roadmap Items
+- Incorporate an MTP library to let the code do the copying directly to the phone and skip the duplicating files to a folder and then copying it over.
+- Add a GUI frontend, possibly packing it up in an Electron App to make it a lot easier for non devs to use.
+- Fix the known problem.
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+/**
+ *  ** APPLE MUSIC PLAYLIST EXPORTER **
+ *  VERSION: 0.1.0 - MLP
+ *  AUTHOR: Greg Bulmash (bulmash.com))
+ *  © 2025 Greg Bulmash - All Rights Reserved
+ *  License: Apache 2.0
+ *  
+ * Reads the XML file exported by the Apple Music app when you ask it to export the Library. 
+ * Preps a folder of music files and M3U playlists you can copy to an Android device.
+ * 
+ * See readme for commands and usage.
+ * 
+ */
+
+
 const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 var parseString = require('xml2js').parseString;
-
 const args = process.argv.slice(2);
 
 var playlists = [];
 var playlist_list = {};
-var action = "";
-var songs = [];
+var songslist = [];
 
 if((args.length == 0) && args[0] !== "analyze" && args[0] !== "export") {
   console.log("Do you want to analyze or export?");
@@ -16,15 +30,41 @@ if((args.length == 0) && args[0] !== "analyze" && args[0] !== "export") {
 
 main();
 
+/** 
+ * 
+ * 
+ *                            MAIN BODY OF THE PROGRAM
+ * 
+ *  
+ **/
+
 async function main(){
+  // the analyze function is needed by both command line args
   let prelim = await analyze();
+
+  // analyze argument - generates a JSON list of playlists
+
   if(args[0] === "analyze"){
     let lists = await JSON.stringify(playlist_list, null, 4);
     fs.writeFileSync("./playlists.json", lists);
     console.log("File analyzed, lists in playlists.json");
     process.exit(1)
   }
-  // all the stuff to make the playlists and copy the files
+
+
+  // create argument - writes the folder of songs and M3U playlists
+  let lists = [];
+  try {
+    lists = await JSON.parse(fs.readFileSync("./playlists.json"));
+  } catch (e){
+    console.log("The JSON file list failed to parse");
+    process.exit(1);
+  }
+  for(let i = 0; i < lists.length; i++){
+    let list = lists[i];
+    console.log("Creating playlist: " + list.Name);
+    let foo = await makeTransfer(list);
+  }
 }
 
 async function analyze(){
@@ -37,7 +77,7 @@ async function analyze(){
       process.exit(1);
     };
     let presongs = result.plist.dict[0].dict[0].dict;
-    songs = await extractSongs(presongs);
+    songslist = await extractSongs(presongs);
     let lists = result.plist.dict[0].array[0].dict; 
     playlist_list = await extractListNames(lists); 
     playlists = await extractListvals(lists);
@@ -45,17 +85,32 @@ async function analyze(){
   return true;
 }
 
-async function writeListofLists(pdata){
+function makeTransfer(list){
+  let listpath = process.env.OUTPUT_PATH;
+  let m3u = "";
+  let playfile = list.Name + ".m3u";
+  let songs = playlists[list.ID][0].dict;
 
+  for(let u = 0; u<songs.length; u++){  
+    let song = songs[u].integer[0];
+    let songpath = songslist[song];
+    // make it work on Mac
+    songpath = songpath.replace("file://", "");
+    songpath = decodeURI(songpath);
+    let filename = path.basename(songpath);
+    // copy file
+    let topath = listpath + "/" + filename;
+    if(!fs.existsSync(songpath)){ // some paths don't always work
+      continue;
+    }
+    if(!fs.existsSync(topath)){ // avoid errors trying to copy the same file twice
+      fs.copyFileSync(songpath, topath);
+    }
+    m3u += filename + "\n";  
+  }
+  fs.writeFileSync(listpath + "/" + playfile, m3u);
 }
 
-async function copyFiles(pdata){
-
-}
-
-async function writeLists(pdata){
-
-}
 
 function extractSongs(slist){
   let songs = [];
@@ -80,10 +135,7 @@ async function extractListvals(slist){
     let list = slist[i];
     let listid = list.integer[0];
     let songids = list["array"];
-    listvals.push({
-      "ID": listid,
-      "Songs": songids.dict
-    }) 
+    listvals[listid] = songids
   }
   return listvals;  
 }
